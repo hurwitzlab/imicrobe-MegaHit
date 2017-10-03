4cb0b7a09dc170f3b5b2d631dd3b1f3cffab5518
Joshua Lynch <jklynch@email.arizona.edu>
2017-10-03 14:54:32 -0700

    initial commit

---
+++ stampede/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -xe
+
+$INPUT_FILE=$1
+$OUTPUT_DIR=$2
+
+module load singularity
+
+#export LAUNCHER_DIR="$HOME/src/launcher"
+#export LAUNCHER_PLUGIN_DIR=$LAUNCHER_DIR/plugins
+#export LAUNCHER_WORKDIR=$OUTPUT_DIR
+#export LAUNCHER_RMI=SLURM
+
+#export LAUNCHER_JOB_FILE=`pwd`/launcher_jobfile
+#echo ${LAUNCHER_JOB_FILE}
+
+singularity run imicrobe-MegaHit.img
+
+#$LAUNCHER_DIR/paramrun
+#echo "Ended launcher"
---
+++ stampede/test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SBATCH -A iPlant-Collabs
+#SBATCH -N 1
+#SBATCH -n 16
+#SBATCH -t 00:30:00
+#SBATCH -p development
+#SBATCH -J test-imicrobe-megahit
+#SBATCH --mail-type BEGIN,END,FAIL
+#SBATCH --mail-user jklynch@email.arizona.edu
+
+module load irods
+
+INPUT_DIR="$SCRATCH/imicrobe-megahit/test/input"
+if [[ -d $INPUT_DIR ]]; then
+  rm -rf $INPUT_DIR
+fi
+
+mkdir -p $INPUT_DIR
+iget /iplant/home/shared/imicrobe/projects/264/samples/5279/E_coli_S_flexneri_0.001.fa $INPUT_DIR
+
+INPUT_FILE=${INPUT_DIR}/E_coli_S_flexneri_0.001.fa
+
+OUTPUT_DIR="$SCRATCH/imicrobe-megahit/test/output"
+if [[ -d $OUTPUT_DIR ]]; then
+  rm -rf $OUTPUT_DIR
+fi
+
+mkdir -p $OUTPUT_DIR
+
+./run.sh $INPUT_FILE $OUTPUT_DIR
