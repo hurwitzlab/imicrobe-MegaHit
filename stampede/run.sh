0b003ca17ba598cef9b2ab231d1a041aa5721049
Joshua Lynch <jklynch@email.arizona.edu>
2017-10-11 16:17:48 -0700

    correct syntax

--- stampede/run.sh
+++ stampede/run.sh
@@ -2,7 +2,7 @@
 
 module load singularity
 
-MEGAHIT_CMD_LINE_ARGS=`singularity exec imicrobe-megahit.img python3 /scripts/agave_to_megahit_cmd_line_args.py $PWD/megahit-out @$`
+MEGAHIT_CMD_LINE_ARGS=`singularity exec imicrobe-megahit.img python3 /scripts/agave_to_megahit_cmd_line_args.py $@`
 
 echo "megahit command line args: \"${MEGAHIT_CMD_LINE_ARGS}\""
 
